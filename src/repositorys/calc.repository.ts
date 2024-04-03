b6c79739be762ca96c58460c2532455b68b917cd
Nicollas <132391248+NicollasDavi@users.noreply.github.com>
2024-04-03 18:55:51 -0300

    add Calc repository

---
+++ src/repositorys/calc.repository.ts
@@ -0,0 +1,25 @@
+import { Curso, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class CalcRepositor{
+    async semDesconto(){
+
+    }
+    
+    async comDesconto(){
+
+    }
+
+    async aVista(){
+
+    }
+
+    async aVistaComDesconto(){
+
+    }
+
+    async calcularInverso(){
+
+    }
+}
--- src/repositorys/user.repository.ts
+++ src/repositorys/user.repository.ts
@@ -79,5 +79,4 @@ class UserRepository{
         }
     }
 }
-
 export default new UserRepository();
