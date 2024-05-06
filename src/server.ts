e1441b5b8168c94b337ef09e89eea60b6b7dd7ee
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-06 15:32:26 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -13,16 +13,26 @@ app.register(fastifyCors, {
 
 app.register(fastifyReplyFrom);
 
-
 app.register(routes);
+
+// Obtém o endereço IP do servidor
+const os = require('os');
+const interfaces = os.networkInterfaces();
+let ipAddress = '';
+Object.keys(interfaces).forEach((interfaceName) => {
+  interfaces[interfaceName].forEach((networkInterface : any) => {
+    if (networkInterface.family === 'IPv4' && !networkInterface.internal) {
+      ipAddress = networkInterface.address;
+    }
+  });
+});
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
-app.listen({ port: port, host: '0.0.0.0' }, (err, address) => {
+app.listen({ port: port, host: ipAddress }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Server listening on ${address}`);
 });
-
