422f61ef62413aa83686a5c6c8f48025c8a7dbf6
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-09 18:28:00 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import fastify from 'fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyReplyFrom from '@fastify/reply-from';
 import { routes } from './routes';
-import * as fs from 'fs'
-import * as path from 'path';
 
 const app = fastify();
 
@@ -30,13 +28,7 @@ Object.keys(interfaces).forEach((interfaceName) => {
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
-// Caminhos para o certificado SSL e a chave privada
-const sslOptions = {
-  key: fs.readFileSync(path.resolve(__dirname, 'caminho-para-sua-chave-privada.key')),
-  cert: fs.readFileSync(path.resolve(__dirname, 'caminho-para-seu-certificado.crt')),
-};
-
-app.listen({ port: port, host: '127.0.0.1', ...sslOptions }, (err, address) => {
+app.listen({ port: port, host: '127.0.0.1' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
