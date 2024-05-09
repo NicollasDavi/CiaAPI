5dc674315351026d207cfe1fb5ff10906c81661b
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-09 15:18:07 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,18 @@ app.register(fastifyReplyFrom);
 
 app.register(routes);
 
+const os = require('os');
+const interfaces = os.networkInterfaces();
+let ipAddress = '';
+Object.keys(interfaces).forEach((interfaceName) => {
+  interfaces[interfaceName].forEach((networkInterface : any) => {
+    if (networkInterface.family === 'IPv4' && !networkInterface.internal) {
+      ipAddress = networkInterface.address;
+    }
+  });
+});
+
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-const ipAddress = '15.228.38.79';
 
 app.listen({ port: port, host: ipAddress }, (err, address) => {
   if (err) {
