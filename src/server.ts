9e178ec2fac39b677d38898608294d48bd5e4931
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-09 17:18:57 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyReplyFrom from '@fastify/reply-from';
 import { routes } from './routes';
-import fs from 'fs';
+import * as fs from 'fs'
 import path from 'path';
 
 const app = fastify();
