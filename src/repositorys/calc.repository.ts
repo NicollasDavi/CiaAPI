25294685184f6e4e795c208b01f4158b454e1e07
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-28 16:40:35 -0300

    .

--- src/repositorys/calc.repository.ts
+++ src/repositorys/calc.repository.ts
@@ -3,41 +3,85 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 interface CursoData {
-    id: string;
+    nome: string;
     unidade: string;
-    turno: string;
     parcelamento: number;
     desconto: number;
 }
 
 class CalcRepository {
     async calcularMensalidadeDoCurso(data: CursoData) {
-        const { id, unidade, turno, parcelamento, desconto } = data;
+        const { nome, unidade, parcelamento, desconto } = data;
 
         try {
-            const curso = await prisma.cursoValor.findUnique({ where: { id } });
+            const cursos = await prisma.cursoValor.findMany({ 
+                where: { 
+                    nome, 
+                    unidade 
+                } 
+            });
+            console.log(cursos)
 
-            if (!curso) {
-                throw new Error(`Curso com o código ${id} não encontrado.`);
+            if (!cursos || cursos.length === 0) {
+                throw new Error(`Cursos com o nome ${nome} na unidade ${unidade} não encontrados.`);
             }
 
-            const { valor_M: Valor_M, valor_E: Valor_E } = curso;
-
-            const calcularMensalidade = (Valor_E: number, Valor_M: number, parcelamento: number, desconto: number): number => {
-                return desconto === 0 
-                    ? (Valor_E + Valor_M) / parcelamento 
-                    : ((Valor_E * (1 - (desconto / 100))) + Valor_M) / parcelamento;
+            const calcularMensalidade = (valorEscola: number, valorMaterial: number, parcelamento: number): number => {
+                return (valorEscola + valorMaterial) / parcelamento;
             };
 
-            const mensalidade = parcelamento === 1 
-                ? Valor_E + Valor_M 
-                : calcularMensalidade(Valor_E, Valor_M, parcelamento, desconto);
+            const calcularMensalidadeDesconto = (valorEscola: number, valorMaterial: number, parcelamento: number, desconto: number): number => {
+                const valorEscolaComDesconto = valorEscola * (1 - (desconto / 100));
+                return (valorEscolaComDesconto + valorMaterial) / parcelamento;
+            };
 
-            const valorEscola = Valor_E.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-            const valorMaterial = Valor_M.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-            const mensalidadeFormatada = mensalidade.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            const resultado = {
+                mensalidadeManha: "",
+                mensalidadeTarde: "",
+                mensalidadeNoite: "",
+                mensalidadeOnline: "",
+                mensalidadeManhaDesconto: "",
+                mensalidadeTardeDesconto: "",
+                mensalidadeNoiteDesconto: "",
+                mensalidadeOnlineDesconto: ""
+            };
 
-            return { mensalidade: mensalidadeFormatada, valorEscola, valorMaterial };
+            cursos.forEach(curso => {
+                const { turno, valor_E: valorEscola, valor_M: valorMaterial } = curso;
+
+                const mensalidade = parcelamento === 1 
+                    ? valorEscola + valorMaterial 
+                    : calcularMensalidade(valorEscola, valorMaterial, parcelamento);
+
+                const mensalidadeDesconto = parcelamento === 1 
+                    ? valorEscola * (1 - (desconto / 100)) + valorMaterial 
+                    : calcularMensalidadeDesconto(valorEscola, valorMaterial, parcelamento, desconto);
+
+                const mensalidadeFormatada = mensalidade.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                const mensalidadeDescontoFormatada = mensalidadeDesconto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+                switch (turno) {
+                    case 'M':
+                        resultado.mensalidadeManha = mensalidadeFormatada;
+                        console.log(resultado.mensalidadeManha)
+                        resultado.mensalidadeManhaDesconto = mensalidadeDescontoFormatada;
+                        break;
+                    case 'T':
+                        resultado.mensalidadeTarde = mensalidadeFormatada;
+                        resultado.mensalidadeTardeDesconto = mensalidadeDescontoFormatada;
+                        break;
+                    case 'N':
+                        resultado.mensalidadeNoite = mensalidadeFormatada;
+                        resultado.mensalidadeNoiteDesconto = mensalidadeDescontoFormatada;
+                        break;
+                    case 'E':
+                        resultado.mensalidadeOnline = mensalidadeFormatada;
+                        resultado.mensalidadeOnlineDesconto = mensalidadeDescontoFormatada;
+                        break;
+                }
+            });
+            console.log(resultado)
+            return resultado;
         } catch (error) {
             console.error('Erro ao calcular mensalidade do curso:', error);
             throw new Error("Ocorreu um erro ao calcular a mensalidade do curso.");
@@ -46,18 +90,18 @@ class CalcRepository {
 
     async calcularInverso(data: any) {
         const { id, unidade, turno, parcelamento, desconto } = data;
-        
+
         try {
             const curso = await prisma.cursoValor.findFirst({ where: { id, unidade, turno } });
-    
+
             if (!curso || !curso.valor_E) {
                 throw new Error('Curso não encontrado ou valor_E não definido.');
             }
-    
+
             const valor_sem_desconto = curso.valor_E / parcelamento;
             const valor_com_desconto = desconto - (curso.valor_M / parcelamento);
             const descontoDado = ((valor_sem_desconto - valor_com_desconto) / valor_sem_desconto) * 100;
-    
+
             return { mensalidade: `${descontoDado.toFixed(2)}%` };
         } catch (error) {
             console.error('Erro ao calcular inverso:', error);
