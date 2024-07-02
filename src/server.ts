bc9fec05b7ba96163da44b4a4a12a34ff9daa08e
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-07-02 13:46:19 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,20 @@ import fastify from 'fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyReplyFrom from '@fastify/reply-from';
 import fastifyMultipart from 'fastify-multipart';
+import fs from 'fs';
 import { routes } from './routes';
 
-const app = fastify();
+const app = fastify({
+  https: {
+    key: fs.readFileSync('/home/nicolas/private.key'),
+    cert: fs.readFileSync('/home/nicolas/certificate.crt'),
+    ca: fs.readFileSync('/home/nicolas/ca_bundle.crt') // Inclua este se necessário
+  }
+});
 
 app.register(fastifyCors, {
-  origin: true, // Permite qualquer origem
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  origin: '*', // Permite qualquer origem
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Permite envio de cookies e cabeçalhos de autenticação
 });
@@ -18,7 +25,7 @@ app.register(fastifyMultipart);
 app.register(routes);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-app.listen({ port: port, host: '127.0.0.1' }, (err, address) => {
+app.listen({ port: port, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
