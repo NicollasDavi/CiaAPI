8f828bee0f6731e67ea8a3faa16c648a26debbc8
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-07-02 13:25:16 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,22 +6,18 @@ import { routes } from './routes';
 
 const app = fastify();
 
-// Configuração do CORS
 app.register(fastifyCors, {
-  origin: 'http://cursopositivocia.com.br', // Especifique o domínio permitido
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Inclua OPTIONS para pré-solicitações
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  preflightContinue: true,
-  optionsSuccessStatus: 204
 });
-
 app.register(fastifyReplyFrom);
 app.register(fastifyMultipart);
 
 app.register(routes);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-app.listen({ port: port, host: '127.0.0.1' }, (err, address) => {
+app.listen({port : port, host: '127.0.0.1'}, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
