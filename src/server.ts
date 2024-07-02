2be3916a89d990352f22e178385365febb394fa7
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-07-02 13:08:45 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { routes } from './routes';
 const app = fastify();
 
 app.register(fastifyCors, {
-  origin: '*',
+  origin: 'http://cursopositivocia.com.br', // Especifique o domínio que você quer permitir
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 });
@@ -17,7 +17,7 @@ app.register(fastifyMultipart);
 app.register(routes);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-app.listen({port : port, host: '127.0.0.1'}, (err, address) => {
+app.listen({ port: port, host: '127.0.0.1' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
