3120000c91d74b43ed3014de6fef78d333991604
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-07-02 13:38:59 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,10 @@ import { routes } from './routes';
 const app = fastify();
 
 app.register(fastifyCors, {
-  origin: '*',
+  origin: true, // Permite qualquer origem
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Permite envio de cookies e cabeçalhos de autenticação
 });
 app.register(fastifyReplyFrom);
 app.register(fastifyMultipart);
@@ -17,7 +18,7 @@ app.register(fastifyMultipart);
 app.register(routes);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-app.listen({port : port, host: '127.0.0.1'}, (err, address) => {
+app.listen({ port: port, host: '127.0.0.1' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
