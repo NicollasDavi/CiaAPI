533c6e206842576a0b4f4e41ba3dce1c7926d161
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-07-04 13:35:44 -0300

    Update limparUpload.ts

--- src/controllers/limparUpload.ts
+++ src/controllers/limparUpload.ts
@@ -1,14 +1,14 @@
-import fs from 'fs';
+import { promises as fsPromises } from 'fs';
 import * as path from 'path';
 
 export const limparDiretorioUploads = async () => {
     const uploadsDir = path.join(__dirname, '../uploads');
 
     try {
-        const arquivos = await fs.promises.readdir(uploadsDir);
+        const arquivos = await fsPromises.readdir(uploadsDir);
         for (const arquivo of arquivos) {
             const filePath = path.join(uploadsDir, arquivo);
-            await fs.promises.unlink(filePath);
+            await fsPromises.unlink(filePath);
         }
         console.log('Conteúdo do diretório de uploads limpo com sucesso');
     } catch (error) {
@@ -20,33 +20,33 @@ export const lerArquivoPDF = async () => {
     try {
         const nomeDoArquivo = 'informativo.pdf';
         const caminhoDoArquivo = path.join(__dirname, `../uploads/${nomeDoArquivo}`);
-        const pdfData = await fs.promises.readFile(caminhoDoArquivo);
+        const pdfData = await fsPromises.readFile(caminhoDoArquivo);
         return pdfData;
     } catch (error) {
         console.error('Erro ao ler o PDF:', error);
         throw new Error('Erro ao ler o PDF');
     }
-}
+};
 
-export const lerArquivo = async (id: string) => {
+export const lerArquivo = async (id : string) => {
     try {
         const nomeDoArquivo = id;
         const caminhoDoArquivo = path.join(__dirname, `../docs/${nomeDoArquivo}`);
-        const pdfData = await fs.promises.readFile(caminhoDoArquivo);
+        const pdfData = await fsPromises.readFile(caminhoDoArquivo);
         return pdfData;
     } catch (error) {
         console.error('Erro ao ler o arquivo:', error);
         throw new Error('Erro ao ler o arquivo');
     }
-}
+};
 
 export const deletarArquivo = async (id: string) => {
     try {
         const caminhoDoArquivo = path.join(__dirname, `../docs/${id}`);
-        await fs.promises.unlink(caminhoDoArquivo);
+        await fsPromises.unlink(caminhoDoArquivo);
         console.log(`Arquivo ${id} deletado com sucesso`);
     } catch (error) {
         console.error(`Erro ao deletar o arquivo ${id}:`, error);
         throw new Error(`Erro ao deletar o arquivo ${id}`);
     }
-}
+};
