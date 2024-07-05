48a20e828dd74fb22c4c5b4e3cc5288a4fcd0fc2
NicollasDavi <nicollasdavisb@gmail.com>
2024-07-04 23:55:40 -0300

    Arrumei as duplicatas

--- src/repositorys/bugfeature.repository.ts
+++ src/repositorys/bugfeature.repository.ts
@@ -1,32 +1,55 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient
+class BugFeatureRepository {
+  async save(data: any) {
+    try {
+      const existingBugOrFeature = await prisma.bugsOrFeatures.findFirst({
+        where: {
+          text: data.text,
+          type: data.type,
+          createdBy: data.createdBy
+        }
+      });
 
-class BugFeatureRepository{
-    async save(data : any){
-        const newBugorFeature = await prisma.bugsOrFeatures.create({
-            data
-        })
-        console.log(newBugorFeature)
-        return newBugorFeature
+      if (existingBugOrFeature) {
+        return { status: 'error', message: 'Um bug ou feature com este texto e tipo já foi criado por este usuário.' };
+      }
+
+      const newBugOrFeature = await prisma.bugsOrFeatures.create({
+        data
+      });
+      console.log(newBugOrFeature);
+      return { status: 'success', newBugOrFeature };
+    } catch (error: any) {
+      throw new Error("Erro ao criar bug ou feature: " + error.message);
     }
+  }
 
-    async get(){
-        const bugsOrFeatures = await prisma.bugsOrFeatures.findMany()
-        console.log(bugsOrFeatures)
-        return bugsOrFeatures
+  async get() {
+    try {
+      const bugsOrFeatures = await prisma.bugsOrFeatures.findMany();
+      console.log(bugsOrFeatures);
+      return bugsOrFeatures;
+    } catch (error: any) {
+      throw new Error("Erro ao buscar bugs ou features: " + error.message);
     }
+  }
 
-    async delete(id : string){
-        const bugsOrFeature = await prisma.bugsOrFeatures.delete({
-            where : {
-                id
-            }
-        })
-        console.log(bugsOrFeature)
-        return bugsOrFeature
+  async delete(id: string) {
+    try {
+      const bugsOrFeature = await prisma.bugsOrFeatures.delete({
+        where: {
+          id
+        }
+      });
+      console.log(bugsOrFeature);
+      return bugsOrFeature;
+    } catch (error: any) {
+      throw new Error("Erro ao deletar bug ou feature: " + error.message);
     }
+  }
 }
 
-export default new BugFeatureRepository()
+export default new BugFeatureRepository();
--- src/repositorys/unidade.repository.ts
+++ src/repositorys/unidade.repository.ts
@@ -1,10 +1,28 @@
 import { PrismaClient } from "@prisma/client";
+import { Mutex } from 'async-mutex';
 
 const prisma = new PrismaClient();
+const mutex = new Mutex();
 
 class UnidadeRepository {
   async save(data: any) {
+    const release = await mutex.acquire();
     try {
+      // Verifique se já existe uma unidade com o mesmo nome, número de telefone ou WhatsApp
+      const existingUnidade = await prisma.unidades.findFirst({
+        where: {
+          OR: [
+            { nome: data.nome },
+            { numeroTel: data.numeroTel },
+            { numeroWpp: data.numeroWpp }
+          ]
+        }
+      });
+
+      if (existingUnidade) {
+        return { status: 'error', message: 'Unidade com este nome ou número de telefone/WhatsApp já existe.' };
+      }
+
       const unidade = await prisma.unidades.create({
         data: {
           horario: data.horario,
@@ -16,103 +34,94 @@ class UnidadeRepository {
           imagem: data.imagem
         },
       });
-      console.log(unidade);
-      return { unidade };
+
+      return { status: 'success', unidade };
     } catch (error) {
       throw error;
+    } finally {
+      release();
     }
   }
 
-  async getAll(){
+  async getAll() {
     try {
       const unidades = await prisma.unidades.findMany({
-        where:{
-          active : true
+        where: {
+          active: true
         }
       });
-      return unidades
+      return unidades;
     } catch (error: any) {
-      throw new Error("Deu rium patrão")
+      throw new Error("Deu rium patrão");
     }
   }
 
-  async getAllAdm(){
+  async getAllAdm() {
     try {
       const unidades = await prisma.unidades.findMany();
-      return unidades
+      return unidades;
     } catch (error: any) {
-      throw new Error("Deu rium patrão")
+      throw new Error("Deu rium patrão");
     }
   }
 
   async getOne(id: string) {
     try {
-        return( await prisma.unidades.findUnique({
-          where : {
-            codigo: id
-          }
-        }))
+      return await prisma.unidades.findUnique({
+        where: {
+          codigo: id
+        }
+      });
     } catch (error) {
-        return null;
+      return null;
     }
-}
+  }
 
-  async delete(id : any){
-    console.log(id)
+  async delete(id: any) {
+    const release = await mutex.acquire();
     try {
       const curso = await prisma.cursoUnidade.findFirst({
-        where:{
-          unidadeId : id
+        where: {
+          unidadeId: id
         }
-      })
-      console.log(curso)
-      if(curso){
-        return {COD: 200, MESSAGE : "A unidade tem cursos vinculados"}
-      }else{
+      });
+
+      if (curso) {
+        return { status: 'error', message: 'A unidade tem cursos vinculados.' };
+      } else {
         const unidade = await prisma.unidades.delete({
           where: {
-            codigo : id
+            codigo: id
           }
-        })
-        return 
+        });
+        return { status: 'success', unidade };
       }
-     
-    }catch (error: any){
-      throw new Error("Erro ao deletar unidade:" + error.errors)
+
+    } catch (error: any) {
+      throw new Error("Erro ao deletar unidade:" + error.message);
+    } finally {
+      release();
     }
   }
 
-  async disable(id: any, action : any){
+  async disable(id: any, action: any) {
+    const release = await mutex.acquire();
     try {
-      if(action == 0){
-        console.log(id)
-        const unidade = await prisma.unidades.update({
-          where: {
-            codigo : id
-          },
-          data: {
-            active : false
-          }
-        })
-        return {unidade}
-      }else{
-        const unidade = await prisma.unidades.update({
-          where: {
-            codigo : id
-          },
-          data: {
-            active : true
-          }
-        })
-        return {unidade}
-      }
-      
+      const unidade = await prisma.unidades.update({
+        where: {
+          codigo: id
+        },
+        data: {
+          active: action === 0 ? false : true
+        }
+      });
+      return { unidade };
     } catch (error: any) {
-      throw new Error("Erro ao desabilitar unidade" + error.erros)
+      throw new Error("Erro ao desabilitar unidade: " + error.message);
+    } finally {
+      release();
     }
   }
-  
-  
 }
 
 export default new UnidadeRepository();
